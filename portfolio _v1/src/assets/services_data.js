5102af19a878123c7bbd688f4113f5a82a7489d9
Pratyush Mohanty <pratyushmohanty7250@gmail.com>
2026-03-20 17:38:53 +0530

    Added new features

---
+++ portfolio _v1/src/assets/services_data.js
@@ -0,0 +1,33 @@
+const Services_Data = [
+    {
+        s_no: "01",
+        s_name: "Web Development",
+        s_desc: "I am a web developer who builds modern, responsive, and high-performance websites using clean code and best practices to deliver seamless user experiences."
+    },
+    {
+        s_no: "02",
+        s_name: "UI/UX Design",
+        s_desc: "I design intuitive and visually engaging user interfaces with a strong focus on user experience, usability, and modern design principles."
+    },
+    {
+        s_no: "03",
+        s_name: "App Design",
+        s_desc: "I design intuitive and user-friendly mobile and web app interfaces focused on usability, accessibility, and smooth interactions."
+    },
+    {
+        s_no: "04",
+        s_name: "Mobile App Development",
+        s_desc: "I build high-performance, scalable mobile applications with clean code, modern UI, and seamless user experience for both Android and iOS."
+    },
+    {
+        s_no: "05",
+        s_name: "Content Writing",
+        s_desc: "I write clear, engaging, and SEO-friendly content tailored to your brand voice — including blogs, product descriptions, and website copy."
+    }, {
+        s_no: "06",
+        s_name: "API Development",
+        s_desc: "I build secure and scalable REST APIs and backend services to power web and mobile applications with efficient data handling."
+    },
+];
+
+export default Services_Data;
